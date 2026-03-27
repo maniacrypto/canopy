feb73d876424b4c8f5997c532846c1a36891edf4
maniacrypt <maniacrypto97@gmail.com>
2026-03-28 00:19:21 +0500

    ether

--- ether.sol
+++ ether.sol
@@ -9,11 +9,11 @@ contract EtherUnits {
     uint256 public oneGwei = 1 gwei;
     // 1 gwei is equal to 10^9 wei
     bool public isOneGwei = (oneGwei == 1e9);
-    //100000000.0
-   
+    //1000000000
+    //0.000000001
 
     uint256 public oneEther = 1 ether;
     // 1 ether is equal to 10^18 wei
     bool public isOneEther = (oneEther == 1e18);
-      //100000000000000000.0
+    //1000000000000000000
 }
